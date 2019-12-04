e989bef17d49cb8b3b4a1265f8c8a8c589801fa4
a-luengen <a-luengen@web.de>
2019-12-04 17:45:24 +0100

    Added  new version for convolution and basic testing python script.

--- opencl/conv2d.cl
+++ opencl/conv2d.cl
@@ -5,18 +5,41 @@ __kernel void sum(__global const float *temp1_g, __global const float *temp2_g, 
         }
 
 
+__kernel void conv2d2(__global float *_kernel, __global int *k_dim, __global float *input, __global int *i_dim, __global float *result)
+{
+    int idx_width = get_global_id(0);
+    int idx_height = get_global_id(1);
+    int w_dim = get_work_dim();
+    int get_global_size();
+    result[]
+    /*
+    int out_width = k_dim[0] - 2;
+    int out_height = k_dim[1] - 2;
+
+    for(int i = 0; i < out_height; i++) 
+    {
+        for(int j = 0; j < out_width; i++) 
+        {
+            result[i + j * out_width] = 0.0;
+        }
+    }
+    result[gid] = 0.0;
+    */
+
+}
+
 __kernel void conv2d(
     __global const float *_kernel, 
-    const unsigned int k_width,
-    const unsigned int k_height,
+    int k_width,
+    unsigned int k_height,
     //const int stride,
     //const int dilation,
     __global const float *input, 
-    const unsigned int in_height,
-    const unsigned int in_width,
+    unsigned int in_height,
+    unsigned int in_width,
     __global float *output,
-    const unsigned int out_height,
-    const unsigned int out_width
+    unsigned int out_height,
+    unsigned int out_width
 ) {
 
     for(unsigned int y = 0; y < (in_width - k_height - 1); y++) 
--- opencl/testOpencl.py
+++ opencl/testOpencl.py
@@ -1,22 +1,21 @@
 import numpy as np 
 import pyopencl as cl
 import os
+import torch
 
+PATH_TO_KERNEL = 'conv2d.cl'
 
-if __name__ == "__main__":
-    
-    os.environ['PYOPENCL_COMPILER_OUTPUT'] = '1'
-    # prevent using cached source code, as this may cause compiler caching failed exception
-    os.environ['PYOPENCL_NO_CACHE'] = '1' 
-    os.environ['PYOPENCL_CTX'] = '0'
-
+def testSum():
     print("Loading kernel...")
-    f = open('testOpencl.cl', 'r', encoding='utf-8')
+    #f = open('testOpencl.cl', 'r', encoding='utf-8')
+    f = open(PATH_TO_KERNEL, 'r', encoding='utf-8')
     kernels = ' '.join(f.readlines())
     f.close()
 
-    temp1_np = np.random.rand(50000000).astype(np.float32)
-    temp2_np = np.random.rand(50000000).astype(np.float32)
+    SIZE = 5
+
+    temp1_np = np.random.rand(SIZE).astype(np.float32)
+    temp2_np = np.random.rand(SIZE).astype(np.float32)
 
     ctx = cl.create_some_context()
     queue = cl.CommandQueue(ctx)
@@ -33,6 +32,80 @@ if __name__ == "__main__":
     result_np = np.empty_like(temp1_np)
     cl.enqueue_copy(queue, result_np, result_g)
 
+    print(temp1_np)
+    print(temp2_np)
+    print(result_np)
     print(result_np - (temp1_np + temp2_np))
     print(np.linalg.norm(result_np - (temp1_np + temp2_np)))
     assert np.allclose(result_np, temp1_np + temp2_np)
+
+def testConvolution():
+    # read kernel file
+    f = open(PATH_TO_KERNEL, 'r', encoding='utf-8')
+    kernels = ' '.join(f.readlines())
+    f.close()
+
+    # create context, queue, buffers and compile kernels
+    ctx = cl.create_some_context()
+    queue = cl.CommandQueue(ctx)
+    mf = cl.mem_flags
+    prg = cl.Program(ctx, kernels).build()
+
+    # init parameters for kernel-call
+    cX = torch.ones(10, 10)
+    print("Input x shape - " + str(cX.shape))
+    print(cX)
+    cKernel = torch.ones(3, 3)
+    print("Input kernel shape - " + str(cKernel.shape))
+    print(cKernel)
+    cOutput = torch.zeros(8, 8)
+    print("Output shape - " + str(cOutput.shape))
+
+    # convert Tensors into usabel np_arrays
+    np_cX = cX.numpy()
+    np_cKernel = cKernel.numpy()
+    np_cOutput = cOutput.numpy()
+    print("Numpy cX - ") 
+    print(str(np_cX))
+    print("Numpy cX dType - " + str(np_cX.dtype))
+    print("Numpy cKernel - ")
+    print(str(np_cKernel))
+    print("Numpy cKernel dType - " + str(np_cKernel.dtype))
+
+    np_dim_cX = np.array(np_cX.shape)
+    print("Dimensions of np_cX - " + str(np_dim_cX))
+    np_dim_cKernel = np.array(np_cKernel.shape)
+    print("Dimensions of np_cKernel - " + str(np_dim_cKernel))
+
+    # copy np_arrays into buffers of device
+    buf_cX = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=np_cX)
+    buf_dim_cX = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=np_dim_cX)
+    buf_cKernel = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=np_cKernel)
+    buf_dim_cKernel = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=np_dim_cKernel)
+    buf_cOutput = cl.Buffer(ctx, mf.WRITE_ONLY, np_cOutput.nbytes)
+
+    #prg.conv2d(queue, np_cOutput.shape, None, 
+    #    buf_cX, np_cX.shape[0], np_cX.shape[1], 
+    #    buf_cKernel, np_cKernel.shape[0], np_cKernel.shape[1],
+    #    buf_cOutput, np_cOutput.shape[0], np_cOutput.shape[1])
+
+    prg.conv2d2(queue, np_cOutput.shape, None,
+        buf_cX, buf_dim_cX,
+        buf_cKernel, buf_dim_cKernel,
+        buf_cOutput)
+    cl.enqueue_copy(queue, np_cOutput, buf_cOutput)
+    print(type(np_cOutput))
+    print(np_cOutput.dtype)
+    print(np_cOutput)
+
+
+if __name__ == "__main__":
+    
+    os.environ['PYOPENCL_COMPILER_OUTPUT'] = '1'
+    # prevent using cached source code, as this may cause compiler caching failed exception
+    os.environ['PYOPENCL_NO_CACHE'] = '1' 
+    os.environ['PYOPENCL_CTX'] = '0'
+
+    testSum()
+    testConvolution()
+
