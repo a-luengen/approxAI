Fixed bias and stride calculation.

net/OCL_Convolution.py
@@ -77,20 +77,17 @@ class OCL_Convolution(nn.Conv2d):
         
         result_tensor = []
 
-        i = 0
-        for out_channel in weight:             
-            t_bias = (0.0) if self.bias is None else self.bias.detach().numpy()[i]
+        for i in range(len(weight)):
+            val_bias = (0.0) if self.bias is None else self.bias.detach().numpy()[i]
             out_channel_result_tensor = self.OCLconvolution(
-                input.detach().numpy(), 
-                out_channel.detach().numpy(),
-                bias=t_bias
+                input.detach().numpy(),
+                weight[i].detach().numpy(),
+                bias=val_bias
             )
             result_tensor.append(torch.tensor(out_channel_result_tensor))
-            i+=1
-
         return torch.stack(result_tensor)
 
-    def OCLconvolution(self, input_3d, kernel_3d, bias=(0.0)):
+    def OCLconvolution(self, input_3d, kernel_3d, bias):
         """
             Takes 3 dimensional input_3d and 3 dimensional
             weight as numpy array to perform the convolution with
@@ -153,7 +150,6 @@ class OCL_Convolution(nn.Conv2d):
             buffer_dim_output,
             buffer_stride,
             buffer_bias)
-        #print("Enqueue Kernel with nd-range shape of output: ", np_output.shape)
         cl.enqueue_nd_range_kernel(queue, convolutionFunct, np_output.shape, None)
         cl.enqueue_copy(queue, np_output, buffer_output)
         return np_output